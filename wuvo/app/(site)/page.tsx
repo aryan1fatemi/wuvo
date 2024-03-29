585f30f669021c4e54dee145a2d7b1244181a056
Ariayn <sf84561@email.vccs.edu>
2024-03-29 13:09:26 -0400

    Auth and landing

---
+++ wuvo/app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from "./components/AuthForm";
+import Landing from "./components/Landing";
+
+export default function Home() {
+    return (
+      <div>
+        <Landing />
+        <AuthForm />
+      </div>
+    );
+  }
