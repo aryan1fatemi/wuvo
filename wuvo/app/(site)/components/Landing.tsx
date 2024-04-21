f65bd4fd5fccf0fb1b97eab21102b10b318648a5
Ariayn <sf84561@email.vccs.edu>
2024-04-21 11:25:11 -0400

    landing page

--- wuvo/app/(site)/components/Landing.tsx
+++ wuvo/app/(site)/components/Landing.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Image from 'next/image'
-import Frame from '../images/logo.png'
+
 const Landing = () => {
   return (
-    <div className='flex flex-col justify-between py-4'>
+    <div className='flex flex-col justify-between py-4 items-center'>
+      <h1 className="bg-gradient-to-r from-orange-900 via-red-500 to-orange-300 inline-block text-transparent bg-clip-text text-7xl text-pretty">This is</h1>
+      <h1 className='bg-gradient-to-r from-orange-900 via-red-500 to-orange-300 inline-block text-transparent bg-clip-text text-9xl text-pretty'>WuVo</h1>
+      <div className='flex items-center align-middle py-5'>
+       <Image
+          height="500"
+          width="500"
+          className="mx-auto w-auto"
+          src='/images/frame.png'
+          alt="Phone"/>
+      </div>
     </div>
   )
 }
