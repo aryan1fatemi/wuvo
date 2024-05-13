de85e5d169c50eb074c59dcede75ecfd31ff48c1
Ariayn <sf84561@email.vccs.edu>
2024-05-12 20:55:26 -0400

    new-landing

--- wuvo/app/(site)/components/Landing.tsx
+++ wuvo/app/(site)/components/Landing.tsx
@@ -5,8 +5,8 @@ const Landing = () => {
   return (
     <div className='flex flex-col justify-between py-4 items-center'>
       <h1 className="bg-gradient-to-r from-orange-900 via-red-500 to-orange-300 inline-block text-transparent bg-clip-text text-7xl text-pretty">This is</h1>
-      <h1 className='bg-gradient-to-r from-orange-900 via-red-500 to-orange-300 inline-block text-transparent bg-clip-text text-9xl text-pretty'>WuVo</h1>
-      <div className='flex items-center align-middle py-5'>
+      <h1 className='bg-gradient-to-r from-orange-900 via-red-500 to-orange-200 inline-block text-transparent bg-clip-text text-9xl text-pretty'>WuVo</h1>
+      <div className='flex items-center py-5 sm:mx-1'>
        <Image
           height="500"
           width="500"
