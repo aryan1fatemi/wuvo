be11a9b99da173254484c9ac3c7b56771976f77a
Ariayn <sf84561@email.vccs.edu>
2024-07-15 15:47:16 -0400

    newauth

--- wuvo/app/api/auth/[...nextauth]/route.ts
+++ wuvo/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,5 @@
-import bcrypt from "bcrypt"
-import NextAuth, { AuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { authOptions } from "@/app/libs/auth";
+import NextAuth from "next-auth";
 
-import prisma from "@/app/libs/prismadb"
-import { authOptions } from "@/app/libs/auth"
-
-/*This code configures authentication options for a 
-Next.js application using NextAuth.js. It sets up multiple authentication providers, including GitHub, Google, and custom credentials,
- and integrates with a Prisma database adapter. */
-
-  
-  const handler = NextAuth(authOptions);
-  
-  export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
