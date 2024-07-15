6909815407a7ce6852d6ce637292dfcfc4786d64
Ariayn <sf84561@email.vccs.edu>
2024-07-15 16:28:59 -0400

    build errors

--- wuvo/next.config.mjs
+++ wuvo/next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:["avatars.githubusercontent.com","res.cloudinary.com", "lh3.googleusercontent.com"]
+    images: {
+      domains: ["avatars.githubusercontent.com", "res.cloudinary.com", "lh3.googleusercontent.com"]
     },
-    ignoreBuildErrors: true
-};
-
-export default nextConfig;
+    typescript: {
+      ignoreBuildErrors: true,
+    }
+  };
+  
+  export default nextConfig;
+  
