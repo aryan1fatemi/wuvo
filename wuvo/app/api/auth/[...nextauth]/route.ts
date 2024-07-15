813d6a08ea0f7f329565969358042b4882048502
Ariayn <sf84561@email.vccs.edu>
2024-07-15 15:27:40 -0400

    auth

--- wuvo/app/api/auth/[...nextauth]/route.ts
+++ wuvo/app/api/auth/[...nextauth]/route.ts
@@ -6,67 +6,12 @@ import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
 import prisma from "@/app/libs/prismadb"
+import { authOptions } from "@/app/libs/auth"
 
 /*This code configures authentication options for a 
 Next.js application using NextAuth.js. It sets up multiple authentication providers, including GitHub, Google, and custom credentials,
  and integrates with a Prisma database adapter. */
 
-export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-      GithubProvider({
-        clientId: process.env.GITHUB_ID as string,
-        clientSecret: process.env.GITHUB_SECRET as string
-      }),
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-      }),
-      CredentialsProvider({
-        name: 'credentials',
-        credentials: {
-          email: { label: 'email', type: 'text' },
-          password: { label: 'password', type: 'password' }
-        },
-        async authorize(credentials) {
-            if (!credentials?.email || !credentials?.password) {
-              throw new Error('Email and password are required');
-            }
-          
-            const user = await prisma.user.findUnique({
-              where: {
-                email: credentials.email
-              }
-            });
-          
-            if (!user) {
-              throw new Error('Email not found');
-            }
-          
-            if (!user.hashedPassword) {
-              throw new Error('Invalid credentials'); // This is a fallback, in case the user record is missing a hashed password
-            }
-          
-            const isCorrectPassword = await bcrypt.compare(
-              credentials.password,
-              user.hashedPassword
-            );
-          
-            if (!isCorrectPassword) {
-              throw new Error('Incorrect password');
-            }
-          
-            return user;
-          }
-          
-      })
-    ],
-    debug: process.env.NODE_ENV === 'development',
-    session: {
-      strategy: "jwt",
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-  }
   
   const handler = NextAuth(authOptions);
   
